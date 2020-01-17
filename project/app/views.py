0d9c2c6c3148cba3115918da4ea9ab8ff10f3b41
WebStar713 <webdeveloper713@gmail.com>
2020-01-17 17:54:39 +0100

    Adding post condition to pass unit test

--- project/app/views.py
+++ project/app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def homePage(request):
+    if request.method == 'POST':
+        return HttpResponse(request.POST['title'])
     return render(request, 'home.html')
