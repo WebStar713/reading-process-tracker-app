ed5603307ef7e45a7f4ded07335e50008444ccb7
WebStar713 <webdeveloper713@gmail.com>
2020-01-17 23:51:39 +0100

    Adding additional 2 request.POST.get

--- project/app/views.py
+++ project/app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def homePage(request):
-    return render(request, 'home.html', {'new_title': request.POST.get('title', ''),})
+    return render(request, 'home.html',
+                    {'new_title': request.POST.get('title', ''),
+                     'new_current_page': request.POST.get('current_page', ''),
+                     'new_total_pages': request.POST.get('total_pages', ''),
+                    })
