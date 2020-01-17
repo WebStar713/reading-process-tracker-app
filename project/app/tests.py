f8111cef12fbc29910241d24612b476fe997e77c
WebStar713 <webdeveloper713@gmail.com>
2020-01-17 18:17:10 +0100

    Fixing error by removing special character

--- project/app/tests.py
+++ project/app/tests.py
@@ -12,5 +12,5 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
     def test_home_page_saves_POST_request(self):
-        response = self.client.post('/', data={'title': 'Some book\'s title'})
-        self.assertIn('Some book\'s title', response.content.decode())
+        response = self.client.post('/', data={'title': 'Some book'})
+        self.assertIn('Some book', response.content.decode())
