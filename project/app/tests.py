293f51570463cebf53f5f7a25f0a5bad184fe505
WebStar713 <webdeveloper713@gmail.com>
2020-01-17 13:16:47 +0100

    Adding second test checking if view function homePage returns correct html code

--- project/app/tests.py
+++ project/app/tests.py
@@ -1,9 +1,18 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+
 from app.views import homePage
 
 class HomePageTest(TestCase):
 
-    def test_root_urls_resolves_to_HomePage_view(self):
+    def test_root_urls_resolves_to_homePage_view(self):
         url_address = resolve('/')
         self.assertEqual(url_address.func, homePage)
+
+    def test_homePage_returns_correct_html(self):
+        request = HttpRequest()
+        response = homePage(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(reponse.content.decode(), expected_html)
