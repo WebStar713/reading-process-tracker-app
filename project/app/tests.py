87ac1e37107da30b8eac08da4ebb18dc7a954e08
WebStar713 <webdeveloper713@gmail.com>
2020-01-17 17:42:40 +0100

    Adding second test checking if post can save

--- project/app/tests.py
+++ project/app/tests.py
@@ -10,3 +10,8 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_saves_POST_request(self):
+        response = self.client.post('/', data={'title': 'Some book\'s title'})
+        self.assertIn('Some book\'s title', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
