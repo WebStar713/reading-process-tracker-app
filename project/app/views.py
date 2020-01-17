2df97ffe7d40b6a97c26adb5962c97a937b5ba2d
WebStar713 <webdeveloper713@gmail.com>
2020-01-17 18:18:26 +0100

    Removing if condition

--- project/app/views.py
+++ project/app/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def homePage(request):
-    if request.method == 'POST':
-        return HttpResponse(request.POST['title'])
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'new_title': request.POST.get('title', ''),})
