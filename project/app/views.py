b553682bb46b65bd3842c25ac42b6cccc6db308c
WebStar713 <webdeveloper713@gmail.com>
2020-01-18 23:11:55 +0100

    Adding if condition to pass unit test

--- project/app/views.py
+++ project/app/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from app.models import Book
 
 def homePage(request):
-    book = Book()
-    book.title = request.POST.get('title', '')
-    book.current_page = request.POST.get('current_page', 0)
-    book.total_pages = request.POST.get('total_pages', 0)
-    book.save()
+    if request.method == 'POST':
+        book = Book()
+        book.title = request.POST.get('title', '')
+        book.current_page = request.POST.get('current_page', 0)
+        book.total_pages = request.POST.get('total_pages', 0)
+        book.save()
+    else:
+        book = Book()
 
     return render(request, 'home.html',
                     {'new_title': book.title,
