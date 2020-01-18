a972a487f9f36151ea3b41cfea62dc6ac59d70ac
WebStar713 <webdeveloper713@gmail.com>
2020-01-18 01:18:33 +0100

    Adding third test checking possibility to save 2 books

--- project/app/tests.py
+++ project/app/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from app.views import homePage
+from app.models import Book
 
 class HomePageTest(TestCase):
 
@@ -20,3 +21,24 @@ class HomePageTest(TestCase):
         self.assertIn('Some book', response.content.decode())
         self.assertIn('125', response.content.decode())
         self.assertIn('317', response.content.decode())
+
+    def test_saves_and_retrieves_lots_books_details(self):
+        first_book = Book()
+        first_book.title = 'Title of first book'
+        first_book.current_page = '12'
+        first_book.total_pages = '300'
+        first_book.save()
+
+        second_book = Book()
+        second_book.title = 'Title of second book'
+        second_book.current_page = '70'
+        second_book.total_pages = '566'
+        second_book.save()
+
+        saved_books = Book.objects.all()
+        self.assertEqual(saved_books.count(), 2)
+
+        first_saved_book = saved_books[0]
+        second_saved_book = saved_books[1]
+        self.assertEqual(first_saved_book, 'Title of first book')
+        self.assertEqual(second_saved_book, 'Title of second book')
