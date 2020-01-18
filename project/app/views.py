0823d7365c312994e2ee6d417e9b4c71938675d3
WebStar713 <webdeveloper713@gmail.com>
2020-01-19 00:18:58 +0100

    Commenting analogous code that not working

--- project/app/views.py
+++ project/app/views.py
@@ -2,12 +2,30 @@ from django.shortcuts import render
 from app.models import Book
 
 def homePage(request):
+    # if request.method == 'POST':
+    #     new_title = request.POST['title']
+    #     new_current_page = request.POST['current_page']
+    #     new_total_pages = request.POST['total_pages']
+    #
+    #     Book.objects.create(title = new_title,
+    #                         current_page = new_current_page,
+    #                         total_pages = new_total_pages,)
+    # else:
+    #     new_title = ''
+    #     new_current_page = 0
+    #     new_total_pages = 0
+    #
+    # return render(request, 'home.html', {'new_title' : new_title,
+    #                                      'new_current_page' : new_current_page,
+    #                                      'new_total_pages' : new_total_pages,
+    #                                     })
+
     if request.method == 'POST':
         book = Book()
         book.title = request.POST.get('title', '')
         book.current_page = request.POST.get('current_page', 0)
         book.total_pages = request.POST.get('total_pages', 0)
-        book.save()
+        book.save() # functional_tests pass only when book.save() is commented
     else:
         book = Book()
 
