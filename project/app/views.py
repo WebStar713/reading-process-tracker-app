72459843a9ac44b5709ce61871095c84231a5665
WebStar713 <webdeveloper713@gmail.com>
2020-01-18 21:38:02 +0100

    Adding object Book and saving it to pass unit test

--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from app.models import Book
 
 def homePage(request):
+    book = Book()
+    book.title = request.POST.get('title', '')
+    book.current_page = request.POST.get('current_page', 0)
+    book.total_pages= request.POST.get('total_pages', 0)
+    book.save()
+
     return render(request, 'home.html',
                     {'new_title': request.POST.get('title', ''),
                      'new_current_page': request.POST.get('current_page', ''),
