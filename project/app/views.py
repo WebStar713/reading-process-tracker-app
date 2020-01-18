3f142f2f1c07f91248e1d097e7b32ae19db7fff6
WebStar713 <webdeveloper713@gmail.com>
2020-01-18 22:37:51 +0100

    Refactoring for return render

--- project/app/views.py
+++ project/app/views.py
@@ -5,11 +5,11 @@ def homePage(request):
     book = Book()
     book.title = request.POST.get('title', '')
     book.current_page = request.POST.get('current_page', 0)
-    book.total_pages= request.POST.get('total_pages', 0)
+    book.total_pages = request.POST.get('total_pages', 0)
     book.save()
 
     return render(request, 'home.html',
-                    {'new_title': request.POST.get('title', ''),
-                     'new_current_page': request.POST.get('current_page', ''),
-                     'new_total_pages': request.POST.get('total_pages', ''),
+                    {'new_title': book.title,
+                     'new_current_page': book.current_page,
+                     'new_total_pages': book.total_pages,
                     })
