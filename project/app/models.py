e931617a207e3a05d15b7fc3afd227ce250c053f
WebStar713 <webdeveloper713@gmail.com>
2020-01-21 11:15:05 +0100

    Adding attribute "list_of_books" to object Book

--- project/app/models.py
+++ project/app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+class ListfOfBooks(models.Model):
+    pass
+
 class Book(models.Model):
     title = models.TextField(default='')
     current_page = models.IntegerField(default=0)
     total_pages = models.IntegerField(default=0)
-
-class ListfOfBooks(models.Model):
-    pass
+    list_of_books = models.TextField(default='')
