ffdda1e8681eb2d32c80a5ebf756af53223f2cf6
WebStar713 <webdeveloper713@gmail.com>
2020-01-21 11:17:35 +0100

    ForeignKey for list_of_books

--- project/app/models.py
+++ project/app/models.py
@@ -7,4 +7,4 @@ class Book(models.Model):
     title = models.TextField(default='')
     current_page = models.IntegerField(default=0)
     total_pages = models.IntegerField(default=0)
-    list_of_books = models.TextField(default='')
+    list_of_books = models.ForeignKey(ListfOfBooks, default=None, on_delete=models.PROTECT)
