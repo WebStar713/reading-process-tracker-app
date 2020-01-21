6ddb1bdd8fcfcaa3c8ab2ca7e77941de91af72c4
WebStar713 <webdeveloper713@gmail.com>
2020-01-21 11:08:12 +0100

    Adding new object list_of_books

--- project/app/tests.py
+++ project/app/tests.py
@@ -12,30 +12,6 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-class BookModelTest(TestCase):
-
-    def test_saves_and_retrieves_lots_books_details(self):
-        first_book = Book()
-        first_book.title = 'Title of first book'
-        first_book.current_page = 12
-        first_book.total_pages = 300
-        first_book.save()
-
-        second_book = Book()
-        second_book.title = 'Title of second book'
-        second_book.current_page = 70
-        second_book.total_pages = 566
-        second_book.save()
-
-        saved_books = Book.objects.all()
-        self.assertEqual(saved_books.count(), 2)
-
-        first_saved_book = saved_books[0]
-        second_saved_book = saved_books[1]
-        self.assertEqual(first_saved_book.title, 'Title of first book')
-        self.assertEqual(second_saved_book.title, 'Title of second book')
-
-
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
@@ -76,3 +52,38 @@ class ListViewTest(TestCase):
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/lists/first-list/')
+
+
+
+class ListAndBookModelTest(TestCase):
+
+    def test_saves_and_retrieves_lots_books_details(self):
+        list_of_books = ListfOfBooks()
+        list_of_books.save()
+
+        first_book = Book()
+        first_book.title = 'Title of first book'
+        first_book.current_page = 12
+        first_book.total_pages = 300
+        first_book.list = list_of_books
+        first_book.save()
+
+        second_book = Book()
+        second_book.title = 'Title of second book'
+        second_book.current_page = 70
+        second_book.total_pages = 566
+        second_book.list = list_of_books
+        second_book.save()
+
+        saved_list_of_books = ListfOfBooks.object.first()
+        self.assertEqual(saved_list_of_books, list_of_books)
+
+        saved_books = Book.objects.all()
+        self.assertEqual(saved_books.count(), 2)
+
+        first_saved_book = saved_books[0]
+        second_saved_book = saved_books[1]
+        self.assertEqual(first_saved_book.title, 'Title of first book')
+        self.assertEqual(first_saved_book.list, list_of_books)
+        self.assertEqual(second_saved_book.title, 'Title of second book')
+        self.assertEqual(second_saved_book.list, list_of_books)
