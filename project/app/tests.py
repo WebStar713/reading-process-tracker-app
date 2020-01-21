08af8262b2cc0817142c2400c835a8acf52aaa06
WebStar713 <webdeveloper713@gmail.com>
2020-01-21 10:52:24 +0100

    Removing useless func

--- project/app/tests.py
+++ project/app/tests.py
@@ -33,9 +33,6 @@ class HomePageTest(TestCase):
         self.assertEqual(first_saved_book.title, 'Title of first book')
         self.assertEqual(second_saved_book.title, 'Title of second book')
 
-    def test_home_page_only_saves_object_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Book.objects.count(), 0)
 
 
 class ListViewTest(TestCase):
