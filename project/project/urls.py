d5a4f61ff1df93ce4a90925dc6df185ccd0c6e9a
WebStar713 <webdeveloper713@gmail.com>
2020-01-21 14:05:25 +0100

    Removing homePage urlpattern

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from app import views
 
 urlpatterns = [
     url(r'^$', views.homePage, name='homePage'),
-    url(r'^lists/(\d+)/$', views.viewList, name='viewList'),
-    url(r'^lists/(\d+)/add_book$', views.addBook, name='addBook'),
-    url(r'^lists/new$', views.newList, name='newList'),
+    url(r'^lists/', include('app.urls')),
 ]
