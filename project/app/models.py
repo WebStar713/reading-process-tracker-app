c3e634c2a68c9706b61ad98927ea8919ac6489d9
WebStar713 <webdeveloper713@gmail.com>
2020-01-21 11:10:41 +0100

    Adding new model ListOfBooks

--- project/app/models.py
+++ project/app/models.py
@@ -4,3 +4,6 @@ class Book(models.Model):
     title = models.TextField(default='')
     current_page = models.IntegerField(default=0)
     total_pages = models.IntegerField(default=0)
+
+class ListfOfBooks(models.Model):
+    pass
