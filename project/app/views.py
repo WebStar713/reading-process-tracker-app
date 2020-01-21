471e9c6bdc2f0ebf60be13629a1c0919f3f9fb5c
WebStar713 <webdeveloper713@gmail.com>
2020-01-21 10:52:01 +0100

    Removing part of homePage code becuase its tasks are done by newList and viewList functions

--- project/app/views.py
+++ project/app/views.py
@@ -2,13 +2,6 @@ from django.shortcuts import render, redirect
 from app.models import Book
 
 def homePage(request):
-    if request.method == 'POST':
-        Book.objects.create(title = request.POST['title'],
-                            current_page = request.POST['current_page'],
-                            total_pages = request.POST['total_pages'],
-                            )
-        return redirect('/lists/first-list/')
-
     return render(request, 'home.html')
 
 def viewList(request):
