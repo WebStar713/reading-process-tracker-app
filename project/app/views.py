937fb0576a13cc6edfe6d6d40dcdfc4dd611242d
WebStar713 <webdeveloper713@gmail.com>
2020-01-21 12:19:54 +0100

    Creating Book object to pass unit test

--- project/app/views.py
+++ project/app/views.py
@@ -20,4 +20,9 @@ def newList(request):
 
 def addBook(request, list_of_books_id):
     list_of_books = ListfOfBooks.objects.get(id=list_of_books_id)
+    Book.objects.create(title = request.POST['title'],
+                        current_page = request.POST['current_page'],
+                        total_pages = request.POST['total_pages'],
+                        list_of_books = list_of_books
+                        )
     return redirect('/lists/%d/' % (list_of_books.id,))
