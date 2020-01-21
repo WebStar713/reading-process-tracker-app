f6a164d3723592f0f1fe225b5f683b02932bf65c
WebStar713 <webdeveloper713@gmail.com>
2020-01-21 12:17:28 +0100

    Adding new url pattern for addBook view

--- project/project/urls.py
+++ project/project/urls.py
@@ -5,5 +5,6 @@ from app import views
 urlpatterns = [
     url(r'^$', views.homePage, name='homePage'),
     url(r'^lists/(\d+)/$', views.viewList, name='viewList'),
+    url(r'^lists/(\d+)/add_book$', views.addBook, name='addBook'),
     url(r'^lists/new$', views.newList, name='newList'),
 ]
