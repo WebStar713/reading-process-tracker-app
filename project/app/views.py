3f9514a9411bc88bf8763a53ac9f5601cf5ddbe4
WebStar713 <webdeveloper713@gmail.com>
2020-01-22 21:09:57 +0100

    Adding userLogin function that returns a LoginForm

--- project/app/views.py
+++ project/app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+
+from app.forms import LoginForm
 from app.models import Book, ListfOfBooks
 
 def homePage(request):
+
     return render(request, 'home.html')
 
 def viewList(request, list_of_books_id):
@@ -42,3 +47,25 @@ def addBook(request, list_of_books_id):
                         list_of_books = list_of_books
                         )
     return redirect(f'/lists/{list_of_books.id}/')
+
+def userLogin(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username = cd['username'],
+                                password = cd['password'])
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated '\
+                                        'successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+
+    return render(request, 'login.html', {'form': form})
