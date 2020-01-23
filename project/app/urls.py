487bb756c5b40c0fee2f0c8e718af2176654dc38
WebStar713 <webdeveloper713@gmail.com>
2020-01-23 15:03:16 +0100

    Replacing url for login - the new one is using a auth_views

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from app import views
 
@@ -6,6 +7,8 @@ urlpatterns = [
     url(r'^lists/(\d+)/$', views.viewList, name='viewList'),
     url(r'^lists/(\d+)/add_book$', views.addBook, name='addBook'),
     url(r'^lists/new$', views.newList, name='newList'),
-    url(r'^login/$', views.userLogin, name='userLogin'),
+    #url(r'^login/$', views.userLogin, name='userLogin'),
     url(r'^logout/$', views.userLogout, name='logout'),
+
+    url('login/', auth_views.LoginView.as_view(), name='login'),
 ]
