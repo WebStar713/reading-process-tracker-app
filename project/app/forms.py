8e6bc0c480ca88101c9101667cdec1d9bd1ac648
WebStar713 <webdeveloper713@gmail.com>
2020-01-23 10:41:33 +0100

    Exchanging forms.Model to forms.Form to pass login process

--- project/app/forms.py
+++ project/app/forms.py
@@ -2,13 +2,13 @@ from django import forms
 
 from django.contrib.auth.models import User
 
-class LoginForm(forms.ModelForm):
+class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         EMPTY_FIELD_ERROR = 'This field is required.'
         model = User
-        fields = ('username', )
+        fields = ('username', 'password')
 
         error_messages = {'username': {'required': EMPTY_FIELD_ERROR}}
