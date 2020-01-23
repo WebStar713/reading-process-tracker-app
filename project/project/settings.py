8eb4eaa99b96db2f409b5ff430449210e60de46c
WebStar713 <webdeveloper713@gmail.com>
2020-01-23 10:42:09 +0100

    Adding admin to be able create a superuser needed for login process

--- project/project/settings.py
+++ project/project/settings.py
@@ -20,7 +20,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app',
-    #'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
