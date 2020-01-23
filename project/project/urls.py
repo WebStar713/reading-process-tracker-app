50ee853756adf4fe71287b9b0b3739028bac479f
WebStar713 <webdeveloper713@gmail.com>
2020-01-23 10:42:57 +0100

    Adding admin/ in urlpatterns to process login

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,8 +1,10 @@
+from django.contrib import admin
 from django.conf.urls import url, include
 
 from app import views
 
 urlpatterns = [
+    url('admin/', admin.site.urls),
     url(r'^$', views.homePage, name='homePage'),
     url('', include('app.urls')),
 ]
