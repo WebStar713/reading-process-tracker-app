ccf641bdb4c5c80ab1771e412478d700dd5ce1ea
WebStar713 <webdeveloper713@gmail.com>
2020-01-24 11:11:28 +0100

    Expanding widgets attributes

--- project/app/forms.py
+++ project/app/forms.py
@@ -3,8 +3,11 @@ from django import forms
 from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':"form_control"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':"form_control"}))
+
+    username.widget.attrs['size'] = '100'
+
 
     class Meta:
         EMPTY_FIELD_ERROR = 'This field is required.'
@@ -12,3 +15,7 @@ class LoginForm(forms.Form):
         fields = ('username', 'password')
 
         error_messages = {'username': {'required': EMPTY_FIELD_ERROR}}
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fields['username'].widget.attrs['class'] = "form_control"
