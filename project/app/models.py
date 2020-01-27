bb7fee3352dd64b38ae09985a267e013f1b5c814
WebStar713 <webdeveloper713@gmail.com>
2020-01-27 15:50:45 +0100

    Adding on_delete=models.SET_DEFAULT to be sure that list of books with incorrect details wont be saved

--- project/app/models.py
+++ project/app/models.py
@@ -10,4 +10,4 @@ class Book(models.Model):
     title = models.TextField(blank=False)
     current_page = models.IntegerField(blank=False)
     total_pages = models.IntegerField(blank=False)
-    list_of_books = models.ForeignKey(ListfOfBooks, default=None, on_delete=models.PROTECT)
+    list_of_books = models.ForeignKey(ListfOfBooks, default=None, on_delete=models.SET_DEFAULT)
