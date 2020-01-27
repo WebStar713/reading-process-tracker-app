564301dfa5e9d1a9a3932f655cee39538581fa50
WebStar713 <webdeveloper713@gmail.com>
2020-01-27 19:39:46 +0100

    Adding missing placeholder and class

--- project/app/tests/test_forms.py
+++ project/app/tests/test_forms.py
@@ -5,4 +5,9 @@ class BookFormTest(TestCase):
 
     def test_form_renders_BookForm_fields_input(self):
         form = BookForm()
-        self.fail(form.as_p())
+
+        self.assertIn('placeholder="Title"', form.as_p())
+        self.assertIn('placeholder="Current page"', form.as_p())
+        self.assertIn('placeholder="Total number of pages"', form.as_p())
+
+        self.assertIn('class="form-control input-lg"', form.as_p())
