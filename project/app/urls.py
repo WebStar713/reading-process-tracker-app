df063ba0be0790ef26d8b4fc315ce52e4403bab6
WebStar713 <webdeveloper713@gmail.com>
2020-01-27 16:51:26 +0100

    Removing ulr pattern for addBook

--- project/app/urls.py
+++ project/app/urls.py
@@ -6,7 +6,6 @@ from app import views
 
 urlpatterns = [
     url(r'^lists/(\d+)/$', views.viewList, name='viewList'),
-    url(r'^lists/(\d+)/add_book$', views.addBook, name='addBook'),
     url(r'^lists/new$', views.newList, name='newList'),
 
 ]
