56360a51a6459c746c782e7b2dc81f70aa19b6c3
WebStar713 <webdeveloper713@gmail.com>
2020-01-27 16:50:51 +0100

    Removing addBook view function

--- project/app/views.py
+++ project/app/views.py
@@ -54,15 +54,6 @@ def newList(request):
 
     return redirect(f'/lists/{list_of_books.id}/')
 
-def addBook(request, list_of_books_id):
-    list_of_books = ListfOfBooks.objects.get(id=list_of_books_id)
-    Book.objects.create(title = request.POST['title'],
-                        current_page = request.POST['current_page'],
-                        total_pages = request.POST['total_pages'],
-                        list_of_books = list_of_books
-                        )
-    return redirect(f'/lists/{list_of_books.id}/')
-
 
 def userLogin(request):
     return render(request, '/login.html')
