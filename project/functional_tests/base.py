a7961f987348fcc58a59ace187b3a84db8dfbc5b
WebStar713 <webdeveloper713@gmail.com>
2020-01-27 11:52:02 +0100

    Only class FunctionalTest here

---
+++ project/functional_tests/base.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+import unittest
+
+class FunctionalTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    @classmethod
+    def tearDown(self):
+        self.browser.quit()
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def check_for_columns_in_book_table(self, test_title, test_current_page, test_total_pages):
+        table = self.browser.find_element_by_id('id_book_table')
+        columns = table.find_elements_by_tag_name('td')
+        self.assertIn(test_title, [column.text for column in columns])
+        self.assertIn(str(test_current_page), [column.text for column in columns])
+        self.assertIn(str(test_total_pages), [column.text for column in columns])
