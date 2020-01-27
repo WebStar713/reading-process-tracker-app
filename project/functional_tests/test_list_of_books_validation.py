b23341d62a8c220b1df155f7ba0f790ee454cf1a
WebStar713 <webdeveloper713@gmail.com>
2020-01-27 12:28:41 +0100

    Filling in a test scenario

--- project/functional_tests/test_list_of_books_validation.py
+++ project/functional_tests/test_list_of_books_validation.py
@@ -13,15 +13,41 @@ import time
 
 class BookValidationTest(FunctionalTest):
 
-    @skip
     def test_cannot_add_empty_book_details(self):
-        pass
+        self.browser.get(self.live_server_url)
+
         # User tried to type empty value in input boxes
+        button_add_book = self.browser.find_element_by_css_selector('.button_main')
+
+        input_new_book_box = self.browser.find_element_by_id('id_new_book')
+        input_current_page_box = self.browser.find_element_by_id('id_current_page')
+        input_total_pages_box = self.browser.find_element_by_id('id_total_pages')
+
+        input_new_book_box.send_keys('')
+        input_current_page_box.send_keys('')
+        input_total_pages_box.send_keys('')
+        button_add_book.click()
 
         # After that, user noticed info about lack possibility to type empty value
+        error = self.browser.find_element_by_css_selector('.has-error').text
+        self.assertEqual(error, "Field 'current_page' expected a number but got ''")
 
         # User tried once again by entering whatever values into inbput boxes
+        input_new_book_box.send_keys("Secondhand: Travels in the New Global Garage Sale")
+        input_current_page_box.send_keys(12)
+        input_total_pages_box.send_keys(320)
+        button_add_book.click()
 
+        self.check_for_columns_in_book_table("Secondhand: Travels in the New Global Garage Sale", 12, 320)
+
+        # User could fix empty input boxes by entering there some text
+        input_new_book_box.send_keys("1984")
+        input_current_page_box.send_keys(10)
+        input_total_pages_box.send_keys(237)
+        button_add_book.click()
+
+        self.check_for_columns_in_book_table("Secondhand: Travels in the New Global Garage Sale", 12, 320)
+        self.check_for_columns_in_book_table("1984", 10, 237)
 
     # def test_user_cannot_sign_in_after_entering_invalid_data(self):
     #     url = urljoin(self.live_server_url, '/login/')
