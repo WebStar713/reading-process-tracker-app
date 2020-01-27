6d62dbf4633ebda4de974f65a07f9e9112c49a36
WebStar713 <webdeveloper713@gmail.com>
2020-01-27 11:19:25 +0100

    Commenting login func

--- project/functional_tests/tests.py
+++ project/functional_tests/tests.py
@@ -144,27 +144,27 @@ class NewVisitorTest(StaticLiveServerTestCase):
         chart = self.browser.find_element_by_id('bar-chart')
         self.assertTrue(chart)
 
-    def test_user_cannot_sign_in_after_entering_invalid_data(self):
-        url = urljoin(self.live_server_url, '/login/')
-        self.browser.get(url)
-
-        # User goes to login page and realizes that the login form is there
-        expected_login_header = "Login to account"
-        login_header_text = self.browser.find_element_by_tag_name('h3').text
-        self.assertEqual(expected_login_header, login_header_text)
-
-        # User enters invalid username and password and then receives info
-        # about invalid login process
-        input_login = self.browser.find_element_by_id('id_username')
-        input_password = self.browser.find_element_by_id('id_password')
-        button_login = self.browser.find_element_by_css_selector('.button_login')
-
-        input_login.send_keys('username')
-        input_password.send_keys('password')
-        button_login.click()
-
-        invalid_login_text = self.browser.find_element_by_tag_name('body').text
-        self.assertIn("Please enter a correct username and password", invalid_login_text)
+    # def test_user_cannot_sign_in_after_entering_invalid_data(self):
+    #     url = urljoin(self.live_server_url, '/login/')
+    #     self.browser.get(url)
+    #
+    #     # User goes to login page and realizes that the login form is there
+    #     expected_login_header = "Login to account"
+    #     login_header_text = self.browser.find_element_by_tag_name('h3').text
+    #     self.assertEqual(expected_login_header, login_header_text)
+    #
+    #     # User enters invalid username and password and then receives info
+    #     # about invalid login process
+    #     input_login = self.browser.find_element_by_id('id_username')
+    #     input_password = self.browser.find_element_by_id('id_password')
+    #     button_login = self.browser.find_element_by_css_selector('.button_login')
+    #
+    #     input_login.send_keys('username')
+    #     input_password.send_keys('password')
+    #     button_login.click()
+    #
+    #     invalid_login_text = self.browser.find_element_by_tag_name('body').text
+    #     self.assertIn("Please enter a correct username and password", invalid_login_text)
 
     # def test_user_can_sign_in_after_entering_valid_data(self):
     #     url = urljoin(self.live_server_url, '/login/')
