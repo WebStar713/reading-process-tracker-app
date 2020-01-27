b256f38300d71738b8640041036243ede3f7e4e7
WebStar713 <webdeveloper713@gmail.com>
2020-01-27 20:09:53 +0100

    Adding 2 more cases for data validation

--- project/app/tests/test_forms.py
+++ project/app/tests/test_forms.py
@@ -14,6 +14,13 @@ class BookFormTest(TestCase):
 
     def test_validation_for_blank_book_details_input(self):
         form = BookForm(data={'title':'', 'current_page': 45, 'total_pages': 500, })
-
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['title'], ['These fields cannot be blank.'])
+
+        form = BookForm(data={'title':'Some title', 'current_page': '', 'total_pages': '', })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['current_page'], ['These fields cannot be blank.'])
+
+        form = BookForm(data={'title':'Some title', 'current_page': 20, 'total_pages': '', })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['total_pages'], ['These fields cannot be blank.'])
