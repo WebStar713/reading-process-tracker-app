ba4fab50a8d41d2f61e6f1c7c04d3ecdc7727155
WebStar713 <webdeveloper713@gmail.com>
2020-01-27 11:57:01 +0100

    Adding import .base

--- project/functional_tests/test_list_of_books_validation.py
+++ project/functional_tests/test_list_of_books_validation.py
@@ -1,3 +1,5 @@
+from .base import FunctionalTest
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
