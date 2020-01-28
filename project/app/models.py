c216dfccabfe93fcb9aaecada5bd28821cb709bc
WebStar713 <webdeveloper713@gmail.com>
2020-01-28 10:28:36 +0100

    Setting blank=True, null=True to pass unit tests

--- project/app/models.py
+++ project/app/models.py
@@ -12,4 +12,4 @@ class Book(models.Model):
     title = models.TextField(blank=False)
     current_page = models.IntegerField(blank=False)
     total_pages = models.IntegerField(blank=False)
-    list_of_books = models.ForeignKey(ListfOfBooks, default=None, on_delete=models.SET_DEFAULT)
+    list_of_books = models.ForeignKey(ListfOfBooks, default=None, blank=True, null=True, on_delete=models.DO_NOTHING)
