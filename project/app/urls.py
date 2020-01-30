4c2a90da548216b8f867f7c81dbc600dd17818bc
WebStar713 <webdeveloper713@gmail.com>
2020-01-30 15:37:30 +0100

    Adding logout path

--- project/app/urls.py
+++ project/app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^lists/(\d+)/$', views.viewList, name='viewList'),
     url(r'^lists/new$', views.newList, name='newList'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
