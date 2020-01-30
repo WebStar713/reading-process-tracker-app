d3728061d4ab0177eefd69b073c2b2d336828601
WebStar713 <webdeveloper713@gmail.com>
2020-01-30 19:49:52 +0100

    Without changes

--- project/app/admin.py
+++ project/app/admin.py
@@ -4,4 +4,4 @@ from .models import Book
 
 @admin.register(Book)
 class BooksAdmin(admin.ModelAdmin):
-    list_display = ('title', 'owner',)
+    list_display = ('title', 'owner', 'list_of_books_id')
