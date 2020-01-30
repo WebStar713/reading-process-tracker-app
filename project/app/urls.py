9dc6f017766fdfb5e91300334c0bf5b2629ddb62
WebStar713 <webdeveloper713@gmail.com>
2020-01-31 00:15:27 +0100

    Adding func of auth_views

--- project/app/urls.py
+++ project/app/urls.py
@@ -7,7 +7,15 @@ from app import views
 urlpatterns = [
     url(r'^lists/(\d+)/$', views.viewList, name='viewList'),
     url(r'^lists/new$', views.newList, name='newList'),
+    url(r'^mylist/$', views.myList, name='myList'),
+
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    url(r'^mylist/$', views.myList, name='myList'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('register/', views.register, name='register'),
 ]
