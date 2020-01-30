cec2ec2ec710765499995c64b5ccf51ee60c7953
WebStar713 <webdeveloper713@gmail.com>
2020-01-30 14:11:57 +0100

    Adding OWNER field for Book model

--- project/app/models.py
+++ project/app/models.py
@@ -13,6 +13,7 @@ class Book(models.Model):
     current_page = models.IntegerField(blank=False)
     total_pages = models.IntegerField(blank=False)
     list_of_books = models.ForeignKey(ListfOfBooks, default=None, blank=True, null=True, on_delete=models.DO_NOTHING)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.DO_NOTHING)
 
     class Meta:
         unique_together = ('list_of_books', 'title')
