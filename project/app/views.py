e841d1ba099949ef267ceeb69783246539f49357
WebStar713 <webdeveloper713@gmail.com>
2020-01-30 19:52:40 +0100

    Filling in myList func

--- project/app/views.py
+++ project/app/views.py
@@ -43,33 +43,49 @@ def viewList(request, list_of_books_id):
 def newList(request):
     form = BookForm(data=request.POST)
     if form.is_valid():
-        print(request.user)
         list_of_books = ListfOfBooks.objects.create()
         form = form.save(for_list=list_of_books)
         form.owner = request.user
+        form.list_of_books = list_of_books
         form = form.save()
         return redirect(list_of_books)
     else:
         return render(request, 'home.html', {"form": form})
 
-def userLogin(request):
+def myList(request):
+
+    list_of_books_set_for_chart = Book.objects.filter(owner = request.user)
+    books_set_ID = []
+    list_of_books_set_ID = []
+    for i in list_of_books_set_for_chart:
+        books_set_ID.append(i.id)
+        list_of_books_set_ID.append(i.list_of_books_id)
+
+    labels = []
+    data = []
+
+    for book in list_of_books_set_for_chart:
+        labels.append(book.title)
+        percentage = round(book.current_page / book.total_pages * 100, 2)
+        data.append(percentage)
+
+    list_of_books = ListfOfBooks.objects.get(id=list_of_books_set_ID[0])
+    form = ExisitingBooksInList(for_list = list_of_books, owner=request.user)
     if request.method == 'POST':
-        form = LoginForm(request.POST)
+        form = ExisitingBooksInList(for_list = list_of_books, owner=request.user, data=request.POST)
         if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username = cd['username'],
-                                password = cd['password'])
+            print(form.owner)
+            print(request.user)
+            form = None
+            form = BookForm(data=request.POST)
+            form = form.save(for_list=list_of_books)
+            form.owner = request.user
+            form.save()
+            return redirect(list_of_books)
+    form = ExisitingBooksInList(for_list = list_of_books, owner=request.user)
 
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authenticated '\
-                                        'successfully')
-                else:
-                    return HttpResponse('Disabled account')
-            else:
-                return HttpResponse('Invalid login')
-    else:
-        form = LoginForm()
-
-    return render(request, '/login.html', {'form': form})
+    return render(request, 'myList.html', {'labels': labels,
+                                          'data': data,
+                                          'list_of_books': list_of_books,
+                                          'form': form,
+                                          })
