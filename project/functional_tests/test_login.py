89d8983f0e3c82fe3226b0c6cfba9d4dee918b68
WebStar713 <webdeveloper713@gmail.com>
2020-02-03 21:53:39 +0100

    Adding test checking for forgotten password form for valid email

--- project/functional_tests/test_login.py
+++ project/functional_tests/test_login.py
@@ -4,6 +4,8 @@ from django.contrib.auth import get_user_model
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
+from private import STAFF_EMAIL
+
 import time
 
 class LoginTest(FunctionalTest):
@@ -25,3 +27,27 @@ class LoginTest(FunctionalTest):
         # User is able to see that he has been logged in
         body = self.browser.find_element_by_tag_name('body').text
         self.assertIn('Hello ' + user.username, body)
+
+class ForgottenPasswordTest(FunctionalTest):
+
+    def test_forgotten_password_form_on_home_page_valid_data(self):
+        # User realizes that home page contains "Forgotten your password?" form
+        self.browser.get(self.live_server_url)
+        body = self.browser.find_element_by_tag_name('body').text
+        self.assertIn('Forgotten your password?', body)
+
+        self.browser.find_element_by_link_text('Forgotten your password?').click()
+
+        # On the subpage where user can restart his password,
+        # there is a window for typing email
+        # User types his email and clicks on button
+        email = self.browser.find_element_by_id('id_email').send_keys(STAFF_EMAIL)
+
+        button_password_book = self.browser.find_element_by_css_selector('.button_login')
+        button_password_book.click()
+
+        # After that user sees information about successful reset process
+        body = self.browser.find_element_by_tag_name('body').text
+        self.assertIn('Instruction to set a new password has been e-mailed to your e-mail address.', body)
+
+    
