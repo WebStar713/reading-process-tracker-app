0c802c7f577f69065546b08c3e3f296b56e73b7d
WebStar713 <webdeveloper713@gmail.com>
2020-02-04 18:02:09 +0100

    Adding func: clean_password2 to check for both passwords are the same

--- project/app/forms.py
+++ project/app/forms.py
@@ -64,3 +64,9 @@ class UserRegistrationForm(forms.ModelForm):
         model = User
         fields = ('username', 'first_name', 'email', 'password', 'password2')
         help_texts = {'username':None}
+
+        def clean_password2(self):
+            cd = self.cleaned_data
+            if cd['password'] != cd['password2']:
+                raise forms.ValidationError('Passwords don\'t match.')
+            return cd['password2']
