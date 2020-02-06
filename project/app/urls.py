75ec1e197a964bfec64039896c606f669953b83c
WebStar713 <webdeveloper713@gmail.com>
2020-02-06 15:40:47 +0100

    Adding url pattern for bookDelete

--- project/app/urls.py
+++ project/app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^lists/(\d+)/$', views.viewList, name='viewList'),
     url(r'^lists/new$', views.newList, name='newList'),
     url(r'^mylist/$', views.myList, name='myList'),
+    url(r'^delete/(?P<pk>[0-9]+)/$', views.bookDelete, name='bookDelete'),
 
     path('', auth_views.LoginView.as_view(), name='login'),
     path('', include('django.contrib.auth.urls')),
